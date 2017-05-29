c63938182b595f7b2c6d447d28575131a7e119ba
marianasena <mariana.sena@treviso.local>
2017-05-29 16:04:13 -0300

    outros

--- resources/views/solicitacao.blade.php
+++ resources/views/solicitacao.blade.php
@@ -47,14 +47,83 @@
 
 
     <form method="post">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="form-group">
-                <label>Funcionário</label>
-                <input type="text" class="form-control" value="" placeholder="Nome do Funcionário" />
+        <div class="panel panel-primary">
+            <div class="panel-heading">Funcionário</div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <label>Nome do Funcionário</label>
+                            <input type="text" class="form-control" value="" placeholder="Nome do Funcionário" />
+                        </div>
+                    </div>
+                </div>
+                <div class="collapse in" id="collapseExample">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Setor</label>
+                                <input type="text" class="form-control" value="" placeholder="26/05/1994" disabled="disabled" />
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Unidade</label>
+                                <input type="text" class="form-control" value="" placeholder="26/05/1994" disabled="disabled" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>Matrícula</label>
+                                <input type="text" class="form-control" value="" placeholder="426201" disabled="disabled" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>Data de Admissão</label>
+                                <input type="text" class="form-control" value="" placeholder="26/05/1994" disabled="disabled" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label>Cargo</label>
+                                <input type="text" class="form-control" value="" placeholder="426201" disabled="disabled" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
+
+        <div class="panel panel-primary">
+            <div class="panel-heading">Desligamento</div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item active col-md-12">
+                                <div class="pull-left icon-select"><span class="glyphicon glyphicon-unchecked"></span></div>
+                                <div class="col-md-11">
+                                    <h4 class="list-group-item-heading">First List Group Item Heading</h4>
+                                    <p class="list-group-item-text">List Group Item Text</p>
+                                </div>
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <h4 class="list-group-item-heading">Second List Group Item Heading</h4>
+                                <p class="list-group-item-text">List Group Item Text</p>
+                            </a>
+                            <a href="#" class="list-group-item">
+                                <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+                                <p class="list-group-item-text">List Group Item Text</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
     </form>
 
